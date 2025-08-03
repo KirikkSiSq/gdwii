7ea83ad23ae7afb54fee09a4f8311b2c8a6b6405
AleFunky <alefunkyextra@gmail.com>
2025-08-03 18:51:42 +0200

    Fix object particles fading

--- source/particles.c
+++ source/particles.c
@@ -737,7 +737,7 @@ void draw_obj_particles(int group_id, GameObject *parent_obj) {
 
     float fade_scale = 1.f;
     
-    float x = ((parent_obj->x - state.camera_x) * SCALE);
+    float x = ((parent_obj->x - state.camera_x) * SCALE) - widthAdjust;
     get_fade_vars(parent_obj, x, &fade_x, &fade_y, &fade_scale);
 
     GX_SetTevOp(GX_TEVSTAGE0, GX_PASSCLR);
