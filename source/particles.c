3873e1a38c231db5ccc4dcecd46278921eac6f27
AleFunky <alefunkyextra@gmail.com>
2025-09-01 17:39:36 +0200

    Opacity easing

--- source/particles.c
+++ source/particles.c
@@ -678,7 +678,7 @@ void update_particles() {
             p->color.r += p->color_delta.r * STEPS_DT;
             p->color.g += p->color_delta.g * STEPS_DT;
             p->color.b += p->color_delta.b * STEPS_DT;
-            p->color.a += p->color_delta.a * STEPS_DT;
+            p->color.a = easeValue(EASE_OUT, p->start_color.a, p->end_color.a, p->elapsed, p->life, 2.f);
             p->elapsed += STEPS_DT;
             if (p->elapsed >= p->life) {
                 p->active = FALSE;
