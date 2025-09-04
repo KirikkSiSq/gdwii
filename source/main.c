7eb7c36b095fd7ccade31623856f3624ef5b6e61
AleFunky <alefunkyextra@gmail.com>
2025-09-04 15:02:01 +0200

    Fix two gfx measurements being 0

--- source/main.c
+++ source/main.c
@@ -121,8 +121,6 @@ void draw_game() {
     int textOffset = (get_text_length(big_font, 0.5, percentage)) / 2;
     draw_text(big_font, big_font_text, screenWidth/2 - textOffset, 10, 0.5, percentage);
     
-    draw_time = 0;
-    obj_particles_time = 0;
 
     if (state.noclip) {
         draw_text(big_font, big_font_text, screenWidth - 200, 20, 0.25, "Noclip activated");
@@ -174,8 +172,10 @@ void draw_game() {
         char cpu_usage[64];
         snprintf(cpu_usage, sizeof(cpu_usage), "CPU: %.2f%%%%", (cpu_time / 16.666666) * 100);
         draw_text(big_font, big_font_text, 20, 400, 0.25, cpu_usage);
-
     }
+    
+    draw_time = 0;
+    obj_particles_time = 0;
 
     if (state.paused) {
         GRRLIB_FillScreen(RGBA(0, 0, 0, 127));
